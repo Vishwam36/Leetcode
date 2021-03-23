356b34ada59dcf97db3d01d077f69c590bd5bbfe
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-03-23 16:12:17 +0530

    Create Monotone Increasing Digits.cpp

---
+++ Greedy/Monotone Increasing Digits.cpp
@@ -0,0 +1,24 @@
+// By Vishwam shriram Mundada
+// https://leetcode.com/problems/monotone-increasing-digits/
+// Easy
+
+class Solution {
+public:    
+    int monotoneIncreasingDigits(int N) 
+    {
+        string s = to_string(N);
+        
+        for(int i = s.size()-2; i >= 0; --i)
+        {
+            if(s[i+1] < s[i])
+            {
+                s[i] -= 1;
+                
+                for(int j = i+1; j < s.size(); ++j)
+                    s[j] = '9';
+            }
+        }
+        
+        return stoi(s);
+    }
+};
