29f54fc26045c5ee3ef1a6fa8569f7a368f72053
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-02 21:57:21 +0530

    Both Recursive and Stack approach

--- Tree Data Structure/Binary Tree Inorder Traversal.cpp
+++ Tree Data Structure/Binary Tree Inorder Traversal.cpp
@@ -13,6 +13,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// App 1 : Recursive
+
 class Solution {
 public:
     vector<int> ans;
@@ -30,6 +33,36 @@ public:
     vector<int> inorderTraversal(TreeNode* root) 
     {
         rec(root);
+        return ans;
+    }
+};
+
+// App 2 : Stack approach
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        vector<int> ans;
+        
+        stack<TreeNode*> s;
+        TreeNode* curr = root;
+        
+        while(!s.empty() || curr != NULL)
+        {
+            if(curr)
+            {
+                s.push(curr);
+                curr = curr->left;
+            }
+            else
+            {
+                curr = s.top();
+                s.pop();
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
         return ans;
     }
 };
