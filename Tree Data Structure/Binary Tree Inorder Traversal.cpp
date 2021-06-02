30b9ff5e1d16c996ba793414b0e5a4a3398df84b
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-06-02 21:12:56 +0530

    Easy, concept need to be known

---
+++ Tree Data Structure/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,35 @@
+// By Vishwam Shriram Mundada
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+// Easy
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    
+    void rec(TreeNode* root)
+    {
+        if(!root)
+            return;
+        
+        rec(root->left);
+        ans.push_back(root->val);
+        rec(root->right);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        rec(root);
+        return ans;
+    }
+};
