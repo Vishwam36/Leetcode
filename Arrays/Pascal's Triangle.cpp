e5397e860d29999a37efe52c7a83e24291e2ccc2
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2022-02-07 14:45:22 +0530

    Easy

---
+++ Arrays/Pascal's Triangle.cpp
@@ -0,0 +1,43 @@
+// By Vishwam Shriram Mundada
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/601/
+// Easy
+
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+
+Constraints:
+    1 <= numRows <= 30
+*/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int n)
+    {
+        vector<vector<int> > v(n);
+        v[0] = {1, 0};
+        for(int i = 1; i < n; ++i)
+        {
+            v[i].push_back(1);
+            
+            for(int j = 1; j <= i; ++j)
+                v[i].push_back(v[i-1][j-1] + v[i-1][j]);
+            
+            v[i].push_back(0);
+        }
+        
+        for(int i = 0; i < n; ++i)
+            v[i].pop_back();
+        
+        return v;
+    }
+};
