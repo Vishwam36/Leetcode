53233213960b2b6aa65ea02adf5e5562a931eaa1
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2022-03-22 17:19:18 +0530

    Good question

--- Binary Search/Search in Rotated Sorted Array.cpp
+++ Binary Search/Search in Rotated Sorted Array.cpp
@@ -31,57 +31,39 @@ nums is guaranteed to be rotated at some pivot.
 -104 <= target <= 104
 */
 
-// First find the index k at which array is rotated
-// then binary search on either halves depending on target
+
+// REF : https://www.youtube.com/watch?v=r3pMQ8-Ad5s
 
 class Solution {
 public:
-    int search(vector<int>& nums, int l, int r, int target)
+    int search(vector<int>& nums, int target)
     {
-        int mid;
-        while(r-l > 1)
+        int n = nums.size();
+        int l = 0, r = n-1, mid;
+        
+        while(r >= l)
         {
             mid = (l+r)/2;
+            
             if(nums[mid] == target)
                 return mid;
             
-            if(target > nums[mid])
-                l = mid+1;
-            else
-                r = mid-1;
+            if(nums[l] <= nums[mid]) // left half is sorted
+            {
+                if(nums[l] <= target && target <= nums[mid])
+                    r = mid-1;
+                else
+                    l = mid+1;
+            }
+            else // right half is sorted
+            {
+                if(nums[mid] <= target && target <= nums[r])
+                    l = mid+1;
+                else
+                    r = mid-1;
+            }
         }
-        if(nums[l] == target)
-            return l;
-        if(nums[r] == target)
-            return r;
+        
         return -1;
-    }
-    
-    int search(vector<int>& nums, int target) 
-    {
-        int l = 0, r = nums.size()-1, mid;
-        if(nums[0] <= nums.back())
-            return search(nums, l, r, target);
-        
-        while(r-l > 1)
-        {
-            mid = (l+r)/2;
-            if(nums[mid] > nums[mid+1])
-                break;
-            
-            if(nums[mid] > nums[0])
-                l = mid+1;
-            else
-                r = mid-1;
-        }
-        if(nums[l] > nums[l+1])
-            mid = l;
-        else if(r < nums.size()-1 && nums[r] > nums[r+1])
-            mid = r;
-        
-        int k = mid;
-        if(target >= nums[0])
-            return search(nums, 0, k, target);
-        return search(nums, k+1, nums.size()-1, target);
     }
 };
