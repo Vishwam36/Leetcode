7ff45d09768973c9c3630dc7657486cb9fa072d3
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2022-04-07 17:23:58 +0530

    Easy

---
+++ DP/Word Break.cpp
@@ -0,0 +1,63 @@
+// By Vishwam Shriram Mundada
+// https://leetcode.com/explore/interview/card/top-interview-questions-hard/121/dynamic-programming/864/
+// Easy
+
+/*
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+
+Constraints:
+    1 <= s.length <= 300
+    1 <= wordDict.length <= 1000
+    1 <= wordDict[i].length <= 20
+    s and wordDict[i] consist of only lowercase English letters.
+    All the strings of wordDict are unique.
+*/
+
+class Solution {
+public:
+    unordered_set<string> dict;
+    vector<int> dp;
+    
+    bool rec(int idx, string& s)
+    {
+        if(idx == s.size())
+            return true;
+        
+        if(dp[idx] != -1)
+            return dp[idx];
+        
+        bool ans = false;
+        for(int sz = 1; idx+sz <= s.size(); ++sz)
+            if(dict.find(s.substr(idx, sz)) != dict.end())
+                ans = ans || rec(idx+sz, s);
+        
+        return dp[idx] = ans;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict)
+    {
+        dict = unordered_set<string>(wordDict.begin(), wordDict.end());
+        dp.resize(s.size()+1, -1);
+        return rec(0, s);
+    }
+};
